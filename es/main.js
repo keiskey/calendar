3b9ce62cdd73e5312f35a6e771249ee451e1c65e
keisukey <keisukey@ranagram.com>
2018-04-02 19:44:27 +0900

    Added files

---
+++ es/main.js
@@ -0,0 +1,8 @@
+const datetime = new Date;
+const month = datetime.getMonth() + 1;
+datetime.setDate(1);
+const startDay = datetime.getDay();
+
+window.addEventListener("DOMContentLoaded", (event) => {
+
+});
---
+++ js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var datetime = new Date();
+var month = datetime.getMonth() + 1;
+datetime.setDate(1);
+var startDay = datetime.getDay();
+
+window.addEventListener("DOMContentLoaded", function (event) {});
