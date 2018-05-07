fe5d23265c727e4f66505f5ad1fd03f3ede97bdc
keisukey <keisukey@ranagram.com>
2018-05-07 17:34:45 +0900

    スタイル調整。

--- es/main.js
+++ es/main.js
@@ -23,28 +23,37 @@ const weeks = Math.ceil((startDay + days) / 7);
 
 window.addEventListener("DOMContentLoaded", (event) => {
   document.querySelector("h1.month").textContent = month;
-  let blank = document.createElement("tr");
-  const table = document.querySelector("table");
-  const dayName = table.querySelector(".header");
+  const tbody = document.querySelector("tbody");
+  const thead = document.querySelector("thead");
   for (let d = START_DAY; d < START_DAY + 7; d++) {
     const name = document.createElement("th");
     name.textContent = DAY_LABELS[d % 7];
-    dayName.appendChild(name);
+    thead.appendChild(name);
   }
-  dayName.appendChild(blank.cloneNode());
+  thead.appendChild(document.createElement("th"));
   let count = 0;
   for (let w = 0; w < weeks; w++) {
     const tr = document.createElement("tr");
     for (let d = START_DAY; d < START_DAY + 7; d++) {
       const td = document.createElement("td");
+      td.classList.toggle(DAY_LABELS[d % 7].toLowerCase());
       tr.appendChild(td);
       if (d + w * 7 < startDay || ++count > days) {
         continue;
       }
-      td.textContent = count;
+      // td.textContent = count;
+      // ----
+      // SVG方式
+      const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+      const text = document.createElementNS(svg.namespaceURI, "text");
+      text.setAttribute("dx", 12);
+      text.setAttribute("dy", 4);
+      text.textContent = count;
+      svg.append(text);
+      td.append(svg);
     }
-    tr.appendChild(blank.cloneNode());
-    table.appendChild(tr);
+    tr.appendChild(document.createElement("td"));
+    tbody.appendChild(tr);
   }
 });
 
--- js/main.js
+++ js/main.js
@@ -25,28 +25,37 @@ var weeks = Math.ceil((startDay + days) / 7);
 
 window.addEventListener("DOMContentLoaded", function (event) {
   document.querySelector("h1.month").textContent = month;
-  var blank = document.createElement("tr");
-  var table = document.querySelector("table");
-  var dayName = table.querySelector(".header");
+  var tbody = document.querySelector("tbody");
+  var thead = document.querySelector("thead");
   for (var d = START_DAY; d < START_DAY + 7; d++) {
     var name = document.createElement("th");
     name.textContent = DAY_LABELS[d % 7];
-    dayName.appendChild(name);
+    thead.appendChild(name);
   }
-  dayName.appendChild(blank.cloneNode());
+  thead.appendChild(document.createElement("th"));
   var count = 0;
   for (var w = 0; w < weeks; w++) {
     var tr = document.createElement("tr");
     for (var _d = START_DAY; _d < START_DAY + 7; _d++) {
       var td = document.createElement("td");
+      td.classList.toggle(DAY_LABELS[_d % 7].toLowerCase());
       tr.appendChild(td);
       if (_d + w * 7 < startDay || ++count > days) {
         continue;
       }
-      td.textContent = count;
+      // td.textContent = count;
+      // ----
+      // SVG方式
+      var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+      var text = document.createElementNS(svg.namespaceURI, "text");
+      text.setAttribute("dx", 12);
+      text.setAttribute("dy", 4);
+      text.textContent = count;
+      svg.append(text);
+      td.append(svg);
     }
-    tr.appendChild(blank.cloneNode());
-    table.appendChild(tr);
+    tr.appendChild(document.createElement("td"));
+    tbody.appendChild(tr);
   }
 });
 
